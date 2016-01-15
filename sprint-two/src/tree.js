a3133f3743502a5c3e58515a3ed8860b70c25279
danmolitor <danmolitor91@gmail.com>
2016-01-15 15:06:19 -0800

    added time complexity predictions

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -41,3 +41,6 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//addChild === O(1) (potentially O(log(n)));
+//contains === O(n);
