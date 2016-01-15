0be6a505f72026d117a2b1b8877c6977ae2db5f3
danmolitor <danmolitor91@gmail.com>
2016-01-15 14:54:13 -0800

    tree data structure passed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,11 +1,12 @@
-var Tree = function(value){
-  var newTree = {};
-  newTree.value = value;
+var Tree = function(value) {
+    var newTree = {};
+    newTree.value = value;
+    _.extend(newTree, treeMethods);
 
-  // your code here
-  newTree.children = null;  // fix me
+    // your code here
+    newTree.children = []; // fix me
 
-  return newTree;
+    return newTree;
 };
 
 
@@ -14,12 +15,26 @@ var Tree = function(value){
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
-
+treeMethods.addChild = function(value) {
+    //We push the new child instance created through Tree, into the children array.
+    this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
-
+treeMethods.contains = function(target) {
+    //Check if the current value is equal to the target.
+    //If it is, return true.
+    if (this.value === target) {
+        return true;
+    }
+    //Iterate through the current instance's children.
+    //Resursively pass in each child and check if its value is equal to target.
+    for (var i = 0; i < this.children.length; i++) {
+        if (this.children[i].contains(target)) {
+            return true;
+        }
+    }
+    //If it does not contain, return false.
+    return false;
 };
 
 
