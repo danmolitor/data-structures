5f44f2eaca2b17b42555121f0b473d748cb77784
danmolitor <danmolitor91@gmail.com>
2016-01-17 12:27:30 -0800

    implemented resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,6 @@
 var HashTable = function() {
     this._limit = 8;
-    //this._count = 0; (Re-sizing)
+    this._count = 0;
     this._storage = LimitedArray(this._limit);
 };
 
@@ -28,9 +28,9 @@ HashTable.prototype.insert = function(key, value) {
     // key exists?
     //update it
     for (var i = 0; i < bucket.length; i++) {
-        var tupal = bucket[i];
-        if (tupal[0] === key) {
-            tupal[1] = value;
+        var tuple = bucket[i];
+        if (tuple[0] === key) {
+            tuple[1] = value;
             found = true;
             break;
         }
@@ -40,6 +40,10 @@ HashTable.prototype.insert = function(key, value) {
     // insert a new tuple
     if (!found) {
         bucket.push([key, value]);
+        this._count++;
+        if (this._count > this._limit * 0.75) {
+            this._resize(this._limit * 2);
+        }
     }
 };
 
@@ -64,9 +68,9 @@ HashTable.prototype.retrieve = function(key) {
     // if tuple[0] === k
     // return tuple[1]
     for (var i = 0; i < bucket.length; i++) {
-        var tupal = bucket[i];
-        if (tupal[0] === key) {
-            return tupal[1];
+        var tuple = bucket[i];
+        if (tuple[0] === key) {
+            return tuple[1];
         }
     }
 
@@ -83,63 +87,78 @@ HashTable.prototype.remove = function(key) {
 
     //Psuedocode from Lecture
 
-    // Retrieve a bucket
+    // Retrieve a bucket and set it to a variable
+
     //If !bucket
-    // return null;
+      // return null;
     var bucket = this._storage.get(idx);
+
     if (!bucket) {
         return null;
     }
+
     // Iterate over bucket
-    //var tuple = bucket[i]
-    // if tuple[0] === k
-    // splice
-    // return tuple[1]
+      //var tuple = bucket[i]
+      // if tuple[0] === k
+        // splice
     for (var i = 0; i < bucket.length; i++) {
-        var tupal = bucket[i];
-        if (tupal[0] === key) {
+        var tuple = bucket[i];
+        if (tuple[0] === key) {
             bucket.splice(i, 1);
-            return tupal[1];
+
+            //Decrement the count by 1
+            this._count--;
+
+            //If the count drops to a quarter of the storage limit
+            if (this._count < this._limit * 0.25) {
+              //We will resize the storage
+                this._resize(this._limit / 2);
+            }
+            // return tuple[1]
+            return tuple[1];
         }
     }
-
+    //return null;
     return null;
-    // return null
 
 };
 
+HashTable.prototype._resize = function(newLimit){
+
+//store the current storage set up of keys and values
+  var oldStorage = this._storage;
+
+  //We set a new set of variables, creating a new storage with the newLimit.
+  this._limit = newLimit;
+  this._storage = LimitedArray(newLimit);
+  this._count = 0;
+
+//Save the current context of this
+  var context = this;
+
+  //Iterate over old storage and re-insert
+  oldStorage.each(function(bucket){
+
+    //iterate over old storage
+if ( !bucket ) {
+  return ;
+}
+
+//for each bucket, we insert the key and value of each tuple
+//back into insert, rehashing them and re placing them.
+    for (var i = 0; i < bucket.length; i++){
+      var tuple = bucket[i];
+      context.insert(tuple[0], tuple[1]);
+    }
+  });
+
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-//var newHash = HashTable();
-//newHash._limit = 8;
-//newHash._storage = LimitedArray(8);
-
-//newHash._storage = var LimitedArray = function(8){
-//                                      var storage = [];
-
-//                                      var limitedArray = {};
-//                                          limitedArray.get = function(index){
-//                                          checkLimit(index);
-//                                          return storage[index];
-//                                        };
-//                                        limitedArray.set = function(index, value){
-//                                          checkLimit(index);
-//                                          storage[index] = value;
-//                                        };
-//                                        limitedArray.each = function(callback){
-//                                          for(var i = 0; i < storage.length; i++){
-//                                            callback(storage[i], i, storage);
-//                                          }
-//                                        };
-
-//                                          var checkLimit = function(index){
-//                                            if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
-//                                            if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
-//                                          };
-
-//                                          return limitedArray;
-//                                        };
+ //Insert  === O(n)
+ //Retrieve === O(n)
+//Remove === O(n)
